d8a725430e2e1dec951675a14de7f606bad88716
Arkadiy28 <84429261+Arkadiy28@users.noreply.github.com>
2021-06-04 12:10:51 +0700

    Create Task_5.2.py

---
+++ Task_5.2.py
@@ -0,0 +1,14 @@
+import numpy
+
+from Modul_Task import series
+from Modul_Task import sr
+
+
+print('Средний рейтинг для жанра "драма":')
+sr(series('драма'))
+
+print()
+
+print('Средний рейтинг для жанра "криминал":')
+sr(series('криминал'))
+
