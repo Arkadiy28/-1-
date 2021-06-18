5ae29eaf3042a5ecef6b59a810916e7d387bf8b8
Arkadiy28 <84429261+Arkadiy28@users.noreply.github.com>
2021-06-18 13:25:02 +0700

    Update Task_8.2.py

--- Task_8.2.py
+++ Task_8.2.py
@@ -7,17 +7,14 @@ response = requests.get(url)
 
 soup = BeautifulSoup(response.text, 'html.parser')
 
-mas = []
-
-for element in soup.find_all('div', class_='tag_count_button'):
-    text = element.text
-    mas.append(text)
-
-def slovar(spisok):
-    qwq = ''.join(spisok)
-    qq = qwq.split()
-    b = {qq[i]: qq[i + 1] for i in range(0, len(qq), 2)}
-    return b
+mas = {}
 
 
-print(slovar(mas))
+for rr in soup.find_all('div', class_='tag_count_button'):
+    name = rr.find('span', class_='tag_name').text
+    tags = None
+    for i in rr.find_all('span', class_='tag_count tab_filter_control_count'):
+        tags = i.text
+    mas[name] = tags
+
+print(mas)
