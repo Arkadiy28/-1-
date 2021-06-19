2c44ea4b53067c95ada890195f0d98b1e69f44a5
Arkadiy28 <84429261+Arkadiy28@users.noreply.github.com>
2021-06-20 03:32:29 +0700

    Update Task_9.3.py

--- Task_9.3.py
+++ Task_9.3.py
@@ -5,6 +5,6 @@ with open('lesson09_cats_of_ulthar.txt', mode='r') as file:
     fil = file.read().split('\n')
     text = str(fil)
 
-cat = re.findall('[к][о][ш][к][А-я]+', text)
+cat = re.findall('кошк[А-я]+', text)
 
 print(len(cat))
