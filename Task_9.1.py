febc688ddf4554b31640f532223c316f34c483aa
Arkadiy28 <84429261+Arkadiy28@users.noreply.github.com>
2021-06-19 19:21:29 +0700

    Create Task_9.1.py

---
+++ Task_9.1.py
@@ -0,0 +1,39 @@
+import re
+
+
+def poisk(spicok, slovo):
+    for i in spicok:
+        b = i[0]
+
+        res = re.search(slovo, b)
+        if res:
+            print(res[0], '-' * 10, b)
+
+fil = []
+
+with open('lesson09_closest_galaxies.txt', encoding = 'utf-8') as file:
+    ee = file.read().split('\n')
+    for i in ee:
+        fil.append(i.split(','))
+
+# Выдаёт все латинские названия
+for i in fil:
+    b = i[0]
+    res = re.match('[A-z]+', b)
+    if res:
+        print(b)
+
+# Выдает названия которые заканчиваются на цифры
+for i in fil:
+    b = i[0]
+    res = re.findall('\s[0-9]+$', b)
+    if res:
+        print(b)
+
+
+poisk(fil, 'Рыбы')
+
+poisk(fil, 'Пегас')
+
+poisk(fil, 'Кит')
+
